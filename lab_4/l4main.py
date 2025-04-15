bc45bab37fb4e2731371298f2de7feb75fed51a3
aengusk <116749459+aengusk@users.noreply.github.com>
2025-04-15 00:06:07 -0400

    Finalize lab 4 path in l4main.py

--- lab_4/l4main.py
+++ lab_4/l4main.py
@@ -18,28 +18,63 @@ def save_data():
         for row in data:
             f.write(f"{row[0]},{row[1]},{row[2]},{row[3]}\n")
 
-def drive_forward(dist, speed = 100): # where dist is in cm and speed is in cm per second
+def drive_forward(dist, speed = 100): # where dist is in [cm] and speed is in [cm / second]
     start_time = time.ticks_ms()
-    drivetrain.set_speed(speed, speed) # 100 cm per second
-    while time.ticks_ms() - start_time < dist/speed * 1000:
+    drivetrain.set_speed(speed, speed)
+    while time.ticks_ms() - start_time < abs(dist/speed) * 1000:
         log()
         time.sleep(0.1)
     drivetrain.stop()
     drivetrain.left_motor.speedController.clear_history()
     drivetrain.right_motor.speedController.clear_history()
 
-def turn(dist, speed = 100): # where angle is in degrees and speed is in cm per second
+def turn(dist, speed = 100): # where dist is in [cm of arc] and speed is in [cm / second]
+    # wheelbase diameter is 15.24 cm
+    # 180 degrees is 23.9389360204 cm of arc
+    # 1 rotation  is 47.8778720407
+    # 2 rotations is 95.7557440814
+    # 8 rotations is 383.022976326
     start_time = time.ticks_ms()
     drivetrain.set_speed(speed, -speed)
-    while time.ticks_ms() - start_time < dist/speed * 1000:
+    while time.ticks_ms() - start_time < abs(dist/speed) * 1000:
         log()
         time.sleep(0.1)
     drivetrain.stop()
     drivetrain.left_motor.speedController.clear_history()
     drivetrain.right_motor.speedController.clear_history()
 
+def experiment():
+
+    # Drive forward 2 meters at high speed
+    drive_forward(200, 150)
+    time.sleep(0.5)
+    
+    # Turn 1.5 rotations at high speed
+    turn(47.8778720407 * 1.5, 150)
+    time.sleep(0.5)
+
+    # Drive forward 5 meters at high speed
+    drive_forward(500, 150)
+    time.sleep(0.5)
+    
+    # Do 2.5 rotations at high speed
+    turn(47.8778720407 * 2.5, 150)
+    time.sleep(0.5)
+    
+    # Final high speed straight run
+    drive_forward(150, 150)
+
+    # Do 8 full rotations at high speed
+    turn(47.8778720407 * 11, 100) # 8 rotations of a 15.24 cm wheelbase is 383.022976326 cm of arc
+
+    # Drive forward 4 meters at medium speed
+    drive_forward(400, 50)
+    time.sleep(0.5)
+
+    save_data()
+
 def main():
-    drive_forward(20, 50)
+    experiment()
 
 if __name__ == '__main__':
     main()

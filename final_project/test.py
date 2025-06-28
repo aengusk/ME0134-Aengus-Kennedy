dd84360aadc8b18f9752f9f6a42fa3098296894c
Aengus Kennedy <116749459+aengusk@users.noreply.github.com>
2025-06-28 01:33:54 -0300

    Finalize final_project/test.py

--- final_project/test.py
+++ final_project/test.py
@@ -1,10 +1,26 @@
-# from XRPLib.deafaults import *
+import time
+from XRPLib.board import Board
 from XRPLib.differential_drive import DifferentialDrive
-from XRPLib.encoded_motor import EncodedMotor
+
+board = Board.get_default_board()
 
 drivetrain = DifferentialDrive.get_default_differential_drive()
-# right_motor = EncodedMotor.get_default_encoded_motor(index=2)
 
-drivetrain.set_speed(1.0, 1.0)
+driving = False
 
-drivetrain.left_motor.speedController.clear_history(); drivetrain.right_motor.speedController.clear_history()
+print('ready to drive...')
+
+while True:
+    if not driving:
+        if board.is_button_pressed():
+            driving = True
+            drivetrain.set_speed(10.0, 10.0)
+            print('set speed 10, 10')
+            time.sleep(0.5)
+    else: # driving
+        if board.is_button_pressed():
+            driving = False
+            drivetrain.set_speed(0.0, 0.0)
+            print('set speed 0, 0')
+            drivetrain.left_motor.speedController.clear_history(); drivetrain.right_motor.speedController.clear_history()
+            time.sleep(0.5)
